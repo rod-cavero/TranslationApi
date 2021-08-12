3e9c66d0f6fac99ccca866647035f1de30b0c06a
rod-cavero <rod.cavero@gmail.com>
2021-08-12 23:23:51 +0200

    default controller

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TranslationApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        private readonly ILogger<DefaultController> _logger;
+
+        public DefaultController(ILogger<DefaultController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        [HttpGet]
+        public string Get()
+        {
+            return "Service Runinng...";
+        }
+
+        
+    }
+}
