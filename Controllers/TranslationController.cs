c378b2e9ec53a5865e2e72c18a37c3d0d10a8abe
rod-cavero <rod.cavero@gmail.com>
2021-08-12 20:46:27 +0200

    comment version

--- Controllers/TranslationController.cs
+++ Controllers/TranslationController.cs
@@ -25,13 +25,16 @@ namespace TranslationApi.Controllers
         [HttpGet]
         public ActionResult<Translation> Get(Translation toTranslate)
         {
-           
+           //call the function to do the translation, passing by reference the class with the data
             if (Translate(ref toTranslate))
             {
+                //the function returns all the information in the same class
+                //which is the one the api return
                 return toTranslate;
             }
             else
             {
+                //if the function fail, raise a internal server error
                 return StatusCode(500);
             }
         }
@@ -46,23 +49,33 @@ namespace TranslationApi.Controllers
                 string uri = String.Format("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
                     toTranslate.SourceLanguage, toTranslate.TargetLanguage, Uri.EscapeUriString(toTranslate.Text));
 
+                //set the client for request
                 using (var httpClient = new HttpClient())
                 {
+                    //make the request to google api
                     var response = httpClient.GetAsync(uri);
                     response.Wait();
 
+                    //get the result from the request
                     var result = response.Result;
                     if (result.IsSuccessStatusCode)
                     {
+                        //if the api call was succesfull extract the data returned
+
                         string dataResult = result.Content.ReadAsStringAsync().Result;
                         var jsonData = JsonSerializer.Deserialize<List<dynamic>>(dataResult);
+                        //take the first element of the list
+                        //where the translation is
                         JsonElement jsonElement = jsonData[0];
+                        //the third element has the source launge in case of request for auto detect
                         toTranslate.SourceLanguage = Convert.ToString(jsonData[2]);
 
+                        //extract every translated sentece to join in a single string
                         foreach (var element in jsonElement.EnumerateArray())
                         {
                             toTranslate.Translated += Convert.ToString(element[0]);
                         }
+                        //every was succesful return true
                         ret = true;
                     }
                 }
--- Translation.cs
+++ Translation.cs
@@ -9,14 +9,14 @@ namespace TranslationApi
     public class Translation
     {
         [Required]
-        public string Text { get; set; }
+        public string Text { get; set; } //Text to be translated
 
         public string SourceLanguage { get; set; }
 
         [Required]
         public string TargetLanguage { get; set; }
 
-        public string Translated { get; set; }
+        public string Translated { get; set; } //Text translated
 
         public Translation()
         {
