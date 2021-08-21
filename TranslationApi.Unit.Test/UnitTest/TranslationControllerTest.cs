5dda2b073b2f7e4e3568ae56d3b5c3284242fa13
Rodrigo Cavero <rod.cavero@gmail.com>
2021-08-22 00:15:06 +0200

    Add unit test

---
+++ TranslationApi.Unit.Test/UnitTest/TranslationControllerTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TranslationApi.Unit.Test
+{
+    [TestClass]
+    public class TranslationControllerTest
+    {
+        [TestMethod]
+        public void Get_WithRightParameters_ReturnTranslation()
+        {
+            //Arrange
+
+            //Act
+
+
+            //Assert
+        }
+
+    }
+}
